cd38878e8662cac13ccd04204cee10976d8338dc
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-26 15:59:21 +0200

    inital commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,31 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.use(express.static('public'));
+
+// creates an absolute path pointing to a folder called "views"
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Index Route:
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+// Random Route:
+app.get('/random', (req, res, next) => {
+  // Retrieve a random chuck joke
+  client.getRandomJoke()
+  .then((response) => {
+    console.log(response);
+  }).catch((err) => {
+    // handle error
+  });
+  
+  res.render('random', { response });
+});
+
+  // Server Started
+app.listen(3000, () => {
+  console.log('Server started, app listening on port 3000!')
+});
