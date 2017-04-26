1ccbbd6b80d4225ab86ba0f5cb373a33d493cddb
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-26 18:43:22 +0200

    fixed the search-form issue.

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const Chuck  = require('chucknorris-io');
+const bodyParser = require('body-parser');
 const client = new Chuck();
 
+
 app.use(express.static('public'));
+
+// instruct the app to use the `bodyParser()` middleware for all routes
+// http://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
 // creates an absolute path pointing to a folder called "views"
 app.set('views', __dirname + '/views');
@@ -19,12 +28,54 @@ app.get('/random', (req, res, next) => {
   // Retrieve a random chuck joke
   client.getRandomJoke()
   .then((response) => {
-    console.log(response);
+    // console.log(response);
+    res.render('random', { response });
   }).catch((err) => {
-    // handle error
+    console.log("The error: ", err);
   });
-  
-  res.render('random', { response });
+});
+
+// List Categories Route:
+app.get('/categories', (req, res, next) => {
+  client.getJokeCategories()
+  .then((response)=>  {
+    // console.log(response);
+    res.render('categories', { response });
+  })
+  .catch((err)=> {
+    console.log("The error: ", err);
+  });
+});
+
+// Unique Category Route:
+app.get('/categories/:categoryId', (req, res, next) => {
+  // Retrieve a random chuck joke
+  const category = req.params.categoryId;
+  client.getRandomJoke(category)
+  .then((response) => {
+    // use the response here
+    // console.log(response);
+    res.render('joke-by-category', { response });
+  }).catch((err) => {
+    console.log("The error: ", err);
+  });
+});
+
+// Search with a keyword for a Joke
+app.get('/search', (req, res, next) => {
+  res.render('search-form', { response: undefined });
+});
+
+// Display Search Result
+app.post('/search', (req, res, next) => {
+  const keyword = req.body.searchKeyword;
+  client.search(keyword)
+  .then(function (response) {
+    // console.log(response);
+    res.render('search-form', { response });
+  }).catch(function (err) {
+    console.log("The error: ", err);
+  });
 });
 
   // Server Started
